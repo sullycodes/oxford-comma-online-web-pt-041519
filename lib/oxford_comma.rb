1dfe089f6270c164658c8ec1259c62ba3a766e70
Daniel Sullivan <dansullivan19@gmail.com>
2019-05-09 20:54:34 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join()
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length == 3
+  i = 0
+  new_arr = []
+    while i < array.length
+      new_arr << array[i]
+      i += 1
+    end
+  new_arr.insert(array.length - 1, "and")
+  new_arr.join(", ")
+  else
+  end
 end
