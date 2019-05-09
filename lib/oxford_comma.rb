844906697de80709054551a63abd9b37c2368cd8
Daniel Sullivan <dansullivan19@gmail.com>
2019-05-09 21:37:37 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join()
+  elsif array.length == 2
+    array.join(" and ")
+  else array.length > 2
+    # array.insert(array.length - 1, "and")
+    array[array.length - 1].prepend("and ")
+    array.join(", ")
+  end
 end
